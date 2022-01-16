7ff4a47c5443edb0d8aa86c7dbd6eda810152436
Karol Szpakowski <karol.szpakowski@gmail.com>
2022-01-16 17:49:50 +0100

    generate gitlab api

--- main.go
+++ main.go
@@ -85,6 +85,13 @@ func main() {
 		setupLog.Error(err, "unable to create controller", "controller", "Project")
 		os.Exit(1)
 	}
+	if err = (&controllers.GitlabReconciler{
+		Client: mgr.GetClient(),
+		Scheme: mgr.GetScheme(),
+	}).SetupWithManager(mgr); err != nil {
+		setupLog.Error(err, "unable to create controller", "controller", "Gitlab")
+		os.Exit(1)
+	}
 	//+kubebuilder:scaffold:builder
 
 	if err := mgr.AddHealthzCheck("healthz", healthz.Ping); err != nil {
